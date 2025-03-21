f23abd138f3a7830c4c5448e47ccb9395ef2264f
suhas-ph <127218737+suhas-ph@users.noreply.github.com>
2025-03-21 19:49:08 +0530

    initial commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  and i love ronaldo \n";
+  cout << "I will become very successfull in life \n";
   return 0;
 }
